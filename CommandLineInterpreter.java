63f7af06bba2e95f8aa177963670e5d9e54566c7
Weird <cfmohammed24@gmail.com>
2024-10-19 19:27:04 +0300

    Initialization

---
+++ CommandLineInterpreter.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class CommandLineInterpreter {
+
+    public static void main(String[] args) {
+        //scanner object to read input
+        boolean isExit = false;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n$> ");
+            String userInput = scanner.nextLine();
+            switch (userInput) {
+                case "ls":
+                    
+                    break;
+            
+                case "cd":
+                    
+                    break;
+                case "pwd":
+                    
+                    break;
+                case "ls -a":
+                    
+                    break;
+                case "ls -r":
+                    
+                    break;
+                case "mkdir":
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+}
+
