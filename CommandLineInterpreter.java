6345f3c820051a1d62c5caa28ae928d84894fe41
Weird <cfmohammed24@gmail.com>
2024-10-19 20:17:16 +0300

    message

--- CommandLineInterpreter.java
+++ CommandLineInterpreter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class CommandLineInterpreter {
-
+    //comment to test the goddamn connection
     public static void main(String[] args) {
         System.out.println("Welcome to the java Unix-based shell...");
         
