7fad084e9e191bf2f7a2c8b117272b1f64956d67
Weird <cfmohammed24@gmail.com>
2024-10-19 20:23:11 +0300

    smth

--- CommandLineInterpreter.java
+++ CommandLineInterpreter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class CommandLineInterpreter {
-    //again
+    //a
     public static void main(String[] args) {
         System.out.println("Welcome to the java Unix-based shell...");
         
